890bf43fbca79743d60baca32f9d7da540a0c682
vsgyou <105686490+vsgyou@users.noreply.github.com>
2024-01-26 16:20:18 +0900

    update

--- prediction.py
+++ prediction.py
@@ -7,6 +7,7 @@ import torch
 import torch.nn as nn
 import yfinance as yf
 import random
+import copy
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from torch.optim.adam import Adam
@@ -51,33 +52,60 @@ num_samples = train_set.x.shape[0]
 # train_x = train_set[:][0]
 # train_y = train_set[:][1]
 
-train_set_org = train_set
+train_set_org = copy.deepcopy(train_set)
 bound,train_trans = pred_minmax(train_set)   # sequence별 max,min저장
 k = 3
 x_pattern = torch.zeros([train_trans.x.shape[0],train_trans.x.shape[1],k])
-#x_pattern_noscale = torch.zeros([train_set.x.shape[0],train_set.x.shape[1],k]) 
+x_pattern_noscale = torch.zeros([train_set_org.x.shape[0],train_set_org.x.shape[1],k]) 
 bound_list = torch.zeros([train_trans.x.shape[0],k+1,2])
 for i in range(num_samples):
-    dist = pd.DataFrame(data = torch.sum(torch.sqrt((train_trans.x[i] - train_trans.x)**2),
-                                         dim = (1,2)), columns = ['Values'])
+    dist = pd.DataFrame(data = torch.sum(torch.sqrt((train_trans.x[i] - train_trans.x)**2), 
+    dim = (1,2)), columns = ['Values'])
     dist_sort = dist.sort_values(by = 'Values')
     ind = dist_sort.index[1:k+1].tolist()
     x_pattern[i,:] = torch.cat([train_trans.x[j] for j in ind],dim = 1)   #[num_sample, seq_len, k]
-    #x_pattern[i,:] = [train_set_org.x[j] for j in ind]
+    x_pattern_noscale[i,:] = torch.cat([train_set_org.x[j] for j in ind], dim=1)
     bound_list[i,:] = torch.cat((torch.FloatTensor(bound[i]).unsqueeze(0),torch.FloatTensor([bound[j] for j in ind])),dim=0)    # [num_sample, k+1, 2] 기존,패턴의 min,max 저장
 torch.FloatTensor(bound)
-train_trans.x.shape
-x_pattern.shape
+
+train_set_org.x = torch.cat([train_set_org.x,x_pattern_noscale],dim = 2)    # minmax 안된 x
+# scaling을 거치고 찾은 패턴을 찾아 변환되기 전의 값으로 합침
+
 train_trans.x = torch.cat([train_trans.x, x_pattern], dim = 2)
-(train_trans.x.shape, train_trans.y.shape)
+
 #%%
 # 데이터 로더
-train_loader = DataLoader(train_trans, batch_size = 64, shuffle = False, drop_last = True)
+train_loader = DataLoader(train_set_org, batch_size = 64, shuffle = False, drop_last = True)
 loader = DataLoader(train_trans, batch_size = 1, shuffle = False, drop_last = True)
 #%%
-# for data,label in train_loader:
-#     data = data
-#     label = label
+for data,label in train_loader:
+    data = data
+    label = label
+
+
+rnn = nn.RNN(input_size = 4, hidden_size = 8, num_layers = 1, batch_first = True)
+h0 = torch.zeros(1,data.shape[0],8)
+fc1 = nn.Linear(in_features = 56, out_features =32)
+fc2 = nn.Linear(in_features = 32, out_features = 1)
+relu = nn.ReLU()
+
+x, hn = rnn(data,h0)
+x = torch.reshape(x, (x.shape[0],-1))
+x.shape
+x = fc1(x)
+x = relu(x)
+x = fc2(x)
+x = torch.flatten(x)
+x.shape
+
+
+
+
+
+
+
+
+
 
 
 #%%
@@ -117,111 +145,3 @@ with torch.no_grad():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-for batch_idx, samples in enumerate(train_loader):
-    x_train, y_train = samples
-
-
-
-#%%
-
-
-
-
-
-#%%
-def calculate_weights(k):
-    # 0부터 1까지를 k개의 구간으로 나누기
-    ranks = np.arange(k, 0, -1) / k
-
-    # 각 구간에 대한 가중치 할당
-    weights = ranks / np.sum(ranks)
-
-    return weights.tolist()
-
-
-k = 10
-X = window(data = input, seq_len = seq_len)
-X.reshape(136,7,1)
-#X = normal(X)
-# %%
-dist = torch.sum(torch.sqrt((X[-1] - X[:-1])**2), dim = (1,2))
-#dist_mat = torch.cat([torch.arange(len(dist)).unsqueeze(1),dist.unsqueeze(1)],dim=1)
-np.array(dist)
-dist_df = pd.DataFrame(dist.numpy(),columns = ['Value'])
-sort_df = dist_df.sort_values(by = 'Value')
-ind = sort_df.iloc[:k].index.tolist()
-
-X_ind = X[ind].reshape(seq_len,k)
-input_data = torch.cat([X[-1],X_ind],dim = 1)
-input_data.reshape(1,7,11)
-
-#%%
-lstm = nn.LSTM(input_size = 7, hidden_size = 11, batch_first = True)
-out,_= lstm(input_data)
-out.shape
-fc = nn.Linear(11,1)
-fc_out = fc(out)
-class SimpleModel(nn.Module):
-    def __init__(self, input_size, hidden_size, output_size):
-        super(SimpleModel, self).__init__()
-        self.lstm = nn.LSTM(input_size, hidden_size, output_size)
-        self.fc = nn.Linear(hidden_size, output_size)
-
-    def forward(self, x):
-        out, _ = self.lstm(x)
-        out = self.fc(out[:,-1,:])
-        return out
-#%%
-
-
-
-
-
-
-
-
-
-
-
-
-
-weight = calculate_weights(k)
-model = SimpleModel(input_size = seq_len, hidden_size = 64, output_size = 1)
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr = 0.001)
-input_data = torch.Tensor(X[ind[:]]).view(-1,seq_len,1)
-scaler = MinMaxScaler()
-scaler.fit(close.values.reshape(-1,1))
-
-target_data  = torch.Tensor(scaler.transform(target.reshape(1,-1)))
-#%%
-num_epochs = 100
-for epoch in range(num_epochs):
-    optimizer.zero_grad()
-    outputs = model(input_data)
-    loss = criterion(outputs, target_data)
-    loss.backward()
-    optimizer.step()
-
-    if (epoch + 1) % 10 == 0:
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-#%%
-model.eval()
-with torch.no_grad():
-    predicted_value = model(input_data)
-
-predicted_value = scaler.inverse_transform(predicted_value.numpy())
-
-
-# %%
