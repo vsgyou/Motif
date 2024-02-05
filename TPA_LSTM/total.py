c664b1081ae75af122e05ca2b32d5c0e765f51db
vsgyou <105686490+vsgyou@users.noreply.github.com>
2024-02-06 01:34:36 +0900

    update

--- TPA_LSTM/total.py
+++ TPA_LSTM/total.py
@@ -3,6 +3,8 @@ import yfinance as yf
 import torch.nn as nn
 import torch
 import pandas as pd
+import tensorflow as tf
+from tensorflow.layers import dense
 from torch.utils.data import DataLoader, Dataset
 from sklearn.model_selection import train_test_split
 #%%
@@ -56,12 +58,82 @@ hn.shape
 cn.shape
 hn[:,:,0]
 
-conv = nn.Conv2d(in_channels = 7,
-                 out_channels = 64,
-                 kernel_size = [6,1],
-                 stride = 1)
 
-input = input.reshape(64,7,6,1)
-conv(input).shape
-input.shape
+
+input = input.reshape(64,7,6)
+
+
 # %%
+attn_size = 8
+attn_length = 7
+batch_size = 64
+
+query = hn
+query = query.squeeze(0)
+query.shape
+atten_states = output
+filter_num = 32
+filter_size = 1
+linear = nn.Linear(query.size(1), filter_num, bias = False)
+w = linear(query)
+w = w.view(-1,1,filter_num)
+atten_states.shape
+reshape_attn_vecs  = atten_states.reshape([batch_size,1,attn_length,attn_size])
+conv = nn.Conv2d(in_channels = 1,
+                 out_channels = 32,
+                 kernel_size = [7,1],
+                 stride = 1)
+conv_vecs = conv(reshape_attn_vecs)
+conv_vecs = conv_vecs.squeeze()
+feature_dim = attn_size - filter_size + 1
+conv_vecs = conv_vecs.reshape([batch_size, feature_dim, filter_num])
+conv_vecs.shape
+
+s = torch.sum(torch.mul(conv_vecs,w),dim = 2)
+s.shape
+sigmoid = nn.Sigmoid()
+a = sigmoid(s)
+a.shape
+
+d = torch.sum(torch.mul(a.reshape([-1,feature_dim,1]),conv_vecs),[1])
+d.shape
+new_conv_vec = d
+con = torch.concat([query,new_conv_vec],axis = 1)
+dense_layer =nn.Linear(con.shape[1],attn_size)
+new_attns = dense_layer(con)
+start_indices = [0,1,0]
+sizes = [-1,-1,-1]
+new_attn_states = atten_states.narrow(1, start_indices[1], sizes[1]).narrow(2, start_indices[2], sizes[2])
+#%%
+class TemporalPatternAttentionMechanism():
+    def __call__(self, hidden, input, batch_size, seq_length, input_size):
+    
+#hidden : output of LSTM (hn) [batch, hidden]
+#input : output of LSTM output [batch, seq_len, hidden]
+        filter_num = 32
+        filter_size = 1
+        Linear1 = nn.Linear(hidden.shape[1], filter_num, bias = False)
+        w = Linear1(hidden).view(-1,1,filter_num)
+        reshape_input = input.reshape([batch_size, 1, seq_length, input_size])
+        conv = nn.Conv2d(in_channels = 1, output_channels = 32, kernel_size = [seq_length, 1], stride = 1)
+        H_c = conv(reshape_input).squeeze()
+        
+        feature_dim = input_size + filter_size - 1
+        H_c = H_c.reshape([batch_size, feature_dim, filter_num])
+
+        s = torch.sum(torch.mul(H_c, w), dim = 2)
+        sigmoid = nn.Sigmoid()
+        a = sigmoid(s)
+
+        v = torch.sum(torch.mul(a.reshape([-1, feature_dim, 1]), H_c), dim = 1)
+        new_h = torch.concat([hidden, v], axis = 1)
+        Linear2 = nn.Linear(new_h.shape[1], input_size)
+        h_prime = Linear2(new_h)
+        
+                
+
+
+
+
+
+
